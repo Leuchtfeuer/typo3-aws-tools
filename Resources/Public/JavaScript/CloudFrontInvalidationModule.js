340623f170c198db77cb437f9304bcf3d556e484
Florian Wessels <flo.wessels@gmail.com>
2020-08-24 08:07:40 +0200

    [TASK] Improve JavaScript

--- Resources/Public/JavaScript/CloudFrontInvalidationModule.js
+++ Resources/Public/JavaScript/CloudFrontInvalidationModule.js
@@ -5,21 +5,26 @@ define(['TYPO3/CMS/Backend/Notification'], function (Notification) {
 
             for (let i = 0, max = actionButtons.length; i < max; i++) {
                 let actionButton = actionButtons[i];
-                actionButton.addEventListener('click', CloudFrontInvalidationModule.ajax, false);
+                actionButton.addEventListener('click', CloudFrontInvalidationModule.invalidate, false);
             }
         },
 
-        ajax: function (event) {
+        invalidate: function (event) {
             const target = event.target.closest('.c-awstools__invalidate');
             const url = target.href;
-            const data = {
+            const body = {
                 type: target.dataset.type,
                 identifier: target.dataset.identifier,
                 storage: target.dataset.storage
             };
 
             target.classList.add('disabled');
+            CloudFrontInvalidationModule.fetch(url, body, target);
 
+            event.preventDefault();
+        },
+
+        fetch: function (url, body, target) {
             fetch(url, {
                 method: 'POST',
                 cache: 'no-cache',
@@ -27,22 +32,21 @@ define(['TYPO3/CMS/Backend/Notification'], function (Notification) {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(data)
+                body: JSON.stringify(body)
             })
-                .then(response => {
-                    const data = response.json();
-                    if (response.status !== 200) {
-                        console.log(data);
-                        throw new Error(data.message);
-                    }
-                    return data;
-                })
+                .then(response => CloudFrontInvalidationModule.parseResponse(response))
                 .then(result => Notification.success(result.title, result.message))
                 .catch(error => Notification.error(error.name, error.message))
                 .finally(() => target.classList.remove('disabled'));
-
-            event.preventDefault();
         },
+
+        parseResponse: function (response) {
+            const data = response.json();
+            if (response.status !== 200) {
+                throw new Error(data.message);
+            }
+            return data;
+        }
     };
 
     CloudFrontInvalidationModule.init();
