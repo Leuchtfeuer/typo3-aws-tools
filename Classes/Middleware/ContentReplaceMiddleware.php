30d60f1d21f24b6c0ec02aad8f2b21e235ed08b3
Fabian Martin <f.martin@leuchtfeuer.com>
2023-06-21 09:03:03 +0200

    [TASK] Add possibility to deactivate url replacement through eventlistener or middleware

--- Classes/Middleware/ContentReplaceMiddleware.php
+++ Classes/Middleware/ContentReplaceMiddleware.php
@@ -21,13 +21,13 @@ class ContentReplaceMiddleware implements MiddlewareInterface
     {
         $language = $request->getAttribute('language')->toArray();
         $response = $handler->handle($request);
+        $config = $GLOBALS['TSFE']->config['config']['tx_awstools.'] ?? [];
 
-        if (filter_var($language['awstools_cdn_enabled'], FILTER_VALIDATE_BOOLEAN) === false || empty($language['awstools_cdn_host'])) {
+        if (filter_var($language['awstools_cdn_enabled'], FILTER_VALIDATE_BOOLEAN) === false || empty($language['awstools_cdn_host']) || $config['replacer.']['middleware'] !== '1') {
             return $response;
         }
 
         $host = rtrim($language['awstools_cdn_host'], '/');
-        $config = $GLOBALS['TSFE']->config['config']['tx_awstools.'] ?? [];
         $patterns = [];
         $replacements = [];
 
