297796a95a48da86c4182cf33d3c0c6b8f98facf
Andreas Engel <a.engel@bitmotion.de>
2024-01-10 11:23:22 +0100

    [TASK] Raise requirements to TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,11 @@
 $EM_CONF['aws_tools'] = [
     'title' => 'Amazon Web Services (AWS) Toolbox',
     'description' => 'This extension connects your TYPO3 instance to Amazon CloudFront. It rewrites all file paths in the frontend to match your CDN domain. You also have the possibility to invalidate Amazon CloudFront entries.',
-    'version' => '11.0.1',
+    'version' => '12.0.0',
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
