cd5831ec319b097be4c12cb6a64e99d98dcf7561
engel <a.engel@leuchtfeuer.com>
2025-04-28 14:21:05 +0200

    [TASK] Initial commit for v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,11 @@
 $EM_CONF['aws_tools'] = [
     'title' => 'Amazon Web Services (AWS) Toolbox',
     'description' => 'This extension connects your TYPO3 instance to Amazon CloudFront. It rewrites all file paths in the frontend to match your CDN domain. You also have the possibility to invalidate Amazon CloudFront entries.',
-    'version' => '12.0.1',
+    'version' => '13.0.0',
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
